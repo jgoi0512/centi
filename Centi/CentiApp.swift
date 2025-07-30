bcbeb958ac6693ba34d92f70d596d8254380e00c
Justin Goi <me@jgoi.net>
2025-07-30 18:45:15 +1000

    Initial Commit

---
+++ Centi/CentiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentiApp.swift
+//  Centi
+//
+//  Created by Justin Goi on 30/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct CentiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Centi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Centi
+//
+//  Created by Justin Goi on 30/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
